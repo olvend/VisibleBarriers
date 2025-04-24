446c084c54421bfddcc8556f1f7f4924f4860800
olvend <olve@nystad.net>
2025-04-24 21:28:51 +0200

    Fix barrier particle reappearing after render toggle without item in hand

--- compat_1_9/src/main/java/io/github/olvend/visiblebarriers/mixin/mixins/BarrierMixin_v9_10_11$.java
+++ compat_1_9/src/main/java/io/github/olvend/visiblebarriers/mixin/mixins/BarrierMixin_v9_10_11$.java
@@ -2,16 +2,22 @@ package io.github.olvend.visiblebarriers.mixin.mixins;
 
 import io.github.olvend.visiblebarriers.VisibleBarriers;
 import net.minecraft.client.particle.Barrier;
+import net.minecraft.client.particle.Particle;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Barrier.class)
-public class BarrierMixin_v12$ {
-    @Inject(method = "Lnet/minecraft/client/particle/Barrier;func_180434_a(Lnet/minecraft/client/renderer/BufferBuilder;Lnet/minecraft/entity/Entity;FFFFFF)V", at = @At("HEAD"), cancellable = true, remap = false)
+public class BarrierMixin_v9_10_11$ extends Particle {
+    protected BarrierMixin_v9_10_11$() {
+        super(null, 0, 0, 0);
+    }
+
+    @Inject(method = "Lnet/minecraft/client/particle/Barrier;func_180434_a(Lnet/minecraft/client/renderer/VertexBuffer;Lnet/minecraft/entity/Entity;FFFFFF)V", at = @At("HEAD"), cancellable = true, remap = false)
     public void removeParticle(CallbackInfo ci) {
         if (VisibleBarriers.isVisible) {
+            this.particleMaxAge = Integer.MIN_VALUE;
             ci.cancel();
         }
     }
--- src/main/java/io/github/olvend/visiblebarriers/mixin/mixins/BarrierMixin_v8$.java
+++ src/main/java/io/github/olvend/visiblebarriers/mixin/mixins/BarrierMixin_v8$.java
@@ -2,16 +2,22 @@ package io.github.olvend.visiblebarriers.mixin.mixins;
 
 import io.github.olvend.visiblebarriers.VisibleBarriers;
 import net.minecraft.client.particle.Barrier;
+import net.minecraft.client.particle.EntityFX;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Barrier.class)
-public class BarrierMixin_v8$ {
+public class BarrierMixin_v8$ extends EntityFX {
+    protected BarrierMixin_v8$() {
+        super(null, 0, 0, 0);
+    }
+
     @Inject(method = "Lnet/minecraft/client/particle/Barrier;func_180434_a(Lnet/minecraft/client/renderer/WorldRenderer;Lnet/minecraft/entity/Entity;FFFFFF)V", at = @At("HEAD"), cancellable = true, remap = false)
     public void removeParticle(CallbackInfo ci) {
         if (VisibleBarriers.isVisible) {
+            this.particleMaxAge = Integer.MIN_VALUE;
             ci.cancel();
         }
     }
